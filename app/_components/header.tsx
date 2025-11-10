4a09b2d435f1c429b85d14f6042109a5460869f0
Vinicyos Ferreira <vinifr987@gmail.com>
2025-11-10 19:06:37 -0300

    feat: add authentication with better-auth

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,30 @@
+"use client";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { LogInIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
 
 const Header = () => {
-    return (
-        <header className="bg-white flex items-center justify-between px-5 py-6">
-            <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
-            <Button variant="outline" size="icon">
-                <MenuIcon/>
-            </Button>
-        </header>
-    );
-}
+  const handleLogin = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+    });
+  };
 
-export default Header
+  return (
+    <header className="flex items-center justify-between bg-white px-5 py-6">
+      <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
+      <div className="flex items-center gap-2">
+        <Button variant="outline" size="icon" onClick={handleLogin}>
+          <LogInIcon />
+        </Button>
+        <Button variant="outline" size="icon">
+          <MenuIcon />
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
