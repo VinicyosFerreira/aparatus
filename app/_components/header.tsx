1cfa7d2a7761160784b281059f5043ef9b8545bf
Vinicyos Ferreira <vinifr987@gmail.com>
2025-11-10 19:16:19 -0300

    feat: add menu component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,26 +2,33 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
-import { LogInIcon } from "lucide-react";
-import { authClient } from "@/lib/auth-client";
+import SidebarMenu from "./sidebar-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 const Header = () => {
-  const handleLogin = async () => {
-    await authClient.signIn.social({
-      provider: "google",
-    });
-  };
-
   return (
     <header className="flex items-center justify-between bg-white px-5 py-6">
       <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
       <div className="flex items-center gap-2">
-        <Button variant="outline" size="icon" onClick={handleLogin}>
-          <LogInIcon />
-        </Button>
-        <Button variant="outline" size="icon">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger>
+            <Button variant="outline" size="icon">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[370px] p-0">
+            <SheetHeader className="border-b px-5 py-6 text-left">
+              <SheetTitle className="text-lg font-bold">Menu</SheetTitle>
+            </SheetHeader>
+            <SidebarMenu />
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
