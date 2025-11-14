19663655ca3ac1f7adfbc55e455d30783eac6abc
Vinicyos Ferreira <vinifr987@gmail.com>
2025-11-13 22:46:20 -0300

    chore: add vercel.json and prisma client reconfig

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,12 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "utfs.io",
       },
-    ]
-  }
+    ],
+  },
+  serverExternalPackages: ["@prisma/client", "prisma"],
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./app/generated/prisma/**/*"],
+  },
 };
 
 export default nextConfig;
